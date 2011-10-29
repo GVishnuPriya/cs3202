747fa849d300b2a396956b5dae295cfdfe95c829
Soares Chen Ruo Fei <crf@hypershell.org>
2011-10-29 17:49:34 +0800

    Tested solver generator.

--- simple/util/solver_generator.h
+++ simple/util/solver_generator.h
@@ -11,37 +11,7 @@ using namespace simple;
 template <typename ConcreteSolver>
 class SimpleSolverGenerator : public QuerySolver  {
   private:
-    class SolverLeftVisitor;
-    class SolverRightVisitor;
-    class FirstSolverVisitor;
-
-    template <typename Condition>
-    class SecondSolverVisitor;
-
-  public:
-    SimpleSolverGenerator(SimpleRoot ast, SolverTable *table) : _solver(ast, table) { }
-
-    virtual ConditionSet solve_left(SimpleCondition *right_condition) {
-        SolverLeftVisitor visitor(&_solver);
-        right_condition->accept_condition_visitor(&visitor);
-        return visitor.return_result();
-    }
-
-    virtual ConditionSet solve_right(SimpleCondition *left_condition) {
-        SolverRightVisitor visitor(&_solver);
-        left_condition->accept_condition_visitor(&visitor);
-        return visitor.return_result();
-    }
-
-    virtual bool validate(SimpleCondition *left_condition, SimpleCondition *right_condition) {
-        FirstSolverVisitor visitor(&_solver, right_condition);
-        left_condition->accept_condition_visitor(&visitor);
-        return visitor.return_result();
-    }
-
-  private:
-    ConcreteSolver _solver;
-
+ 
     class SolverLeftVisitor : public ConditionVisitor {
       public:
         SolverLeftVisitor(ConcreteSolver *solver) : _solver(solver) { }
@@ -100,53 +70,8 @@ class SimpleSolverGenerator : public QuerySolver  {
         ConditionSet _result;
     };
 
-    class ConditionVisitorWithResult : public ConditionVisitor {
-      public:
-        virtual ConditionSet return_result() = 0;
-
-        virtual ~ConditionVisitorWithResult() { }
-    };
-
-    class FirstSolverVisitor : public ConditionVisitor {
-      public:
-        FirstSolverVisitor(ConcreteSolver *solver, SimpleCondition *right_condition) :
-            _solver(solver), 
-            _right_condition(right_condition),
-            _second_visitor(NULL)
-        { }
-
-        void visit_statement_condition(StatementCondition *condition) {
-            _second_visitor = new SecondSolverVisitor<
-                StatementAst>(_solver, condition->get_statement_ast());
-        }
-
-        void visit_proc_condition(ProcCondition *condition) {
-            _second_visitor = new SecondSolverVisitor<
-                ProcAst>(_solver, condition->get_proc_ast());
-        }
-
-        void visit_variable_condition(VariableCondition *condition) {
-            _second_visitor = new SecondSolverVisitor<
-                SimpleVariable>(_solver, condition->get_variable());
-        }
-
-        void visit_pattern_condition(PatternCondition *condition) {
-            _second_visitor = new SecondSolverVisitor<
-                ExprAst>(_solver, condition->get_expr_ast());
-        }
-
-        ConditionSet return_result() {
-            return _second_visitor->return_result();
-        }
-
-      private:
-        ConcreteSolver *_solver;
-        SimpleCondition *_right_condition;
-        std::unique_ptr<ConditionVisitorWithResult> _second_visitor;
-    };
-
     template <typename Condition>
-    class SecondSolverVisitor : public ConditionVisitorWithResult {
+    class SecondSolverVisitor : public ConditionVisitor {
       public:
         SecondSolverVisitor(ConcreteSolver *solver, Condition *condition) :
             _solver(solver), 
@@ -174,44 +99,87 @@ class SimpleSolverGenerator : public QuerySolver  {
                     _left_condition, right_condition->get_expr_ast());
         }
 
-        ConditionSet return_result() {
+        bool return_result() {
             return std::move(_result);
         }
 
       private:
         ConcreteSolver *_solver;
         Condition *_left_condition;
-        ConditionSet _result;
+        bool _result;
 
     };
-};
 
-/*
- * This is a generic solver generator that adds memoization to
- * existing solver class
- */
-template <typename ConcreteSolver>
-class MemoizedSolverGenerator : public QuerySolver  {
+    class FirstSolverVisitor : public ConditionVisitor {
+      public:
+        FirstSolverVisitor(ConcreteSolver *solver, SimpleCondition *right_condition) :
+            _solver(solver), 
+            _right_condition(right_condition)
+        { }
+
+        void visit_statement_condition(StatementCondition *condition) {
+            SecondSolverVisitor<StatementAst> visitor(_solver, condition->get_statement_ast());
+            _right_condition->accept_condition_visitor(&visitor);
+            _result = visitor.return_result();
+        }
+
+        void visit_proc_condition(ProcCondition *condition) {
+            SecondSolverVisitor<ProcAst> visitor(_solver, condition->get_proc_ast());
+            _right_condition->accept_condition_visitor(&visitor);
+            _result = visitor.return_result();
+        }
+
+        void visit_variable_condition(VariableCondition *condition) {
+            SecondSolverVisitor<SimpleVariable> visitor(_solver, condition->get_variable());
+            _right_condition->accept_condition_visitor(&visitor);
+            _result = visitor.return_result();
+        }
+
+        void visit_pattern_condition(PatternCondition *condition) {
+            SecondSolverVisitor<ExprAst> visitor(_solver, condition->get_expr_ast());
+            _right_condition->accept_condition_visitor(&visitor);
+            _result = visitor.return_result();
+        }
+
+        bool return_result() {
+            return std::move(_result);
+        }
+
+      private:
+        bool _result;
+        ConcreteSolver *_solver;
+        SimpleCondition *_right_condition;
+    };
+
   public:
-    MemoizedSolverGenerator(SimpleRoot ast) : _solver(ast) { }
+    SimpleSolverGenerator(SimpleRoot ast, SolverTable *table) : _solver(ast, table) { }
 
     virtual ConditionSet solve_left(SimpleCondition *right_condition) {
-
+        SolverLeftVisitor visitor(&_solver);
+        right_condition->accept_condition_visitor(&visitor);
+        return visitor.return_result();
     }
 
     virtual ConditionSet solve_right(SimpleCondition *left_condition) {
-
+        SolverRightVisitor visitor(&_solver);
+        left_condition->accept_condition_visitor(&visitor);
+        return visitor.return_result();
     }
 
     virtual bool validate(SimpleCondition *left_condition, SimpleCondition *right_condition) {
+        FirstSolverVisitor visitor(&_solver, right_condition);
+        left_condition->accept_condition_visitor(&visitor);
+        return visitor.return_result();
+    }
 
+    ConcreteSolver* get_solver() {
+        return &_solver;
     }
 
   private:
     ConcreteSolver _solver;
-    // table declarations here
-};
 
+};
     
 } // namespace impl
 } // namespace simple
--- test/test_solver.cpp
+++ test/test_solver.cpp
@@ -1,13 +1,110 @@
 
+#include <exception>
 #include "gtest/gtest.h"
-#include "impl/solvers/follows.h"
+#include "impl/ast.h"
+#include "impl/condition.h"
 #include "impl/solvers/modifies.h"
 #include "impl/solvers/next.h"
+#include "simple/util/solver_generator.h"
 
 namespace simple {
 namespace test {
 
+using namespace simple;
+using namespace simple::impl;
+using namespace simple::util;
+
 TEST(SolverTest, BasicTest) {
+    /*
+     * proc test1 {
+     *   if a {
+     *     x = 1;
+     *   } else {
+     *     y = 2;
+     *   }
+     *
+     *   while b {
+     *     z  = 3;
+     *     call test2;
+     *   }
+     * }
+     *
+     * proc test2 {
+     *   a = 4;
+     * }
+     */
+
+    SimpleProcAst *proc1 = new SimpleProcAst("test1");
+    SimpleProcAst *proc2 = new SimpleProcAst("test2");
+
+    SimpleVariable var_x("x");
+    SimpleVariable var_y("y");
+    SimpleVariable var_z("z");
+    SimpleVariable var_a("a");
+
+    SimpleConditionalAst *condition = new SimpleConditionalAst();
+    set_proc(condition, proc1);
+
+    SimpleWhileAst *loop = new SimpleWhileAst();
+    set_next(condition, loop);
+
+    SimpleAssignmentAst *stat1_1 = new SimpleAssignmentAst();
+    stat1_1->set_variable(var_x);
+    stat1_1->set_expr(new SimpleConstAst(1));
+    set_then_branch(stat1_1, condition);
+
+    SimpleAssignmentAst *stat1_2 = new SimpleAssignmentAst();
+    stat1_2->set_variable(var_y);
+    stat1_2->set_expr(new SimpleConstAst(2));
+    set_else_branch(stat1_2, condition);
+
+    SimpleAssignmentAst *stat2_1 = new SimpleAssignmentAst();
+    stat2_1->set_variable(var_z);
+    stat2_1->set_expr(new SimpleConstAst(3));
+    set_while_body(stat2_1, loop);
+
+    SimpleCallAst *stat2_2 = new SimpleCallAst();
+    stat2_2->set_proc_called(proc2);
+    set_next(stat2_1, stat2_2);
+
+    SimpleAssignmentAst *stat3 = new SimpleAssignmentAst();
+    stat3->set_variable(var_a);
+    stat3->set_expr(new SimpleConstAst(4));
+    set_proc(stat3, proc2);
+
+    std::vector<ProcAst*> procs;
+    procs.push_back(proc1);
+    procs.push_back(proc2);
+
+    SimpleRoot root(procs);
+
+    SimpleStatementCondition loop_condition(loop);
+    SimpleStatementCondition stat1_condition(stat1_1);
+    SimpleVariableCondition z_condition(var_z);
+    
+    SimpleSolverGenerator<ModifiesSolver> modifies_solver(root, NULL);
+    SimpleSolverGenerator<NextSolver> next_solver(root, NULL);
+    
+    QuerySolver *modifies_interface = &modifies_solver;
+    EXPECT_EQ((modifies_interface->validate(&loop_condition, &z_condition)), 
+        (modifies_solver.get_solver()->validate<StatementAst, SimpleVariable>(loop, &var_z)));;
+
+    EXPECT_EQ((modifies_interface->solve_right(&loop_condition)), 
+        (modifies_solver.get_solver()->solve_right<StatementAst>(loop)));;
+
+    EXPECT_EQ((modifies_interface->solve_left(&z_condition)), 
+        (modifies_solver.get_solver()->solve_left<SimpleVariable>(&var_z)));;
+
+
+    QuerySolver *next_interface = &next_solver;
+    EXPECT_EQ((next_interface->validate(&stat1_condition, &loop_condition)), 
+        (next_solver.get_solver()->validate<StatementAst, StatementAst>(stat1_1, loop)));;
+
+    EXPECT_EQ((next_interface->solve_right(&stat1_condition)), 
+        (next_solver.get_solver()->solve_right<StatementAst>(stat1_1)));;
+
+    EXPECT_EQ((next_interface->solve_left(&loop_condition)), 
+        (next_solver.get_solver()->solve_left<StatementAst>(loop)));;
 
 }
 
