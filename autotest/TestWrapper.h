7812931be78a97c5b76349b9749e32b7a1398839
Soares Chen <soares.chen@gmail.com>
2014-01-27 02:56:32 +0800

    Added back manually configured Visual Studio project files.

--- autotest/TestWrapper.h
+++ autotest/TestWrapper.h
@@ -6,15 +6,8 @@
 #include <iostream>
 #include <list>
 
+#include "simple/spa.h"
 #include "AbstractWrapper.h"
-
-class SimpleProgramAnalyzer {
-  public:
-    virtual void parse(const std::string& filename) = 0;
-    virtual std::vector<std::string> evaluate(const std::string& query) = 0;
-};
-
-SimpleProgramAnalyzer* create_simple_program_analyzer();
 
 class TestWrapper : public AbstractWrapper {
  public:
