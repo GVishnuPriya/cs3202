2c26aa75fb9297b8e5ed39d539b0f83d10ef95ea
Soares Chen <soares.chen@gmail.com>
2014-04-13 04:56:23 +0800

    Fixed Sibling formatting again.

--- impl/solvers/sibling.cpp
+++ impl/solvers/sibling.cpp
@@ -23,279 +23,280 @@
 #include "simple/util/condition_utils.h"
 
 namespace simple {
-	namespace impl {
+namespace impl {
 
-		using namespace simple;
-		using namespace std;
+using namespace simple;
+using namespace std;
 
-		SiblingSolver::SiblingSolver(SimpleRoot ast) : _ast(ast) {
-			ProcSet proc_set;
+SiblingSolver::SiblingSolver(SimpleRoot ast) : _ast(ast) {
+    ProcSet proc_set;
 
-			//Construct a complete Set on all the procedure in the source code
-			for (auto it = _ast.begin(); it!= _ast.end(); ++it) {
-				proc_set.insert(*it);
-				index_proc(*it);
-			}
+    //Construct a complete Set on all the procedure in the source code
+    for (auto it = _ast.begin(); it!= _ast.end(); ++it) {
+        proc_set.insert(*it);
+        index_proc(*it);
+    }
 
-			//Fill up the Sibling table for procedures
-			for(auto it = proc_set.begin(); it!=proc_set.end(); ++it){
-				ProcSet proc_sibling_set = proc_set;
-				proc_sibling_set.erase(*it);
-				_sibling_proc_index[*it] = proc_sibling_set;
-			}
-		}
+    //Fill up the Sibling table for procedures
+    for(auto it = proc_set.begin(); it!=proc_set.end(); ++it){
+        ProcSet proc_sibling_set = proc_set;
+        proc_sibling_set.erase(*it);
+        _sibling_proc_index[*it] = proc_sibling_set;
+    }
+}
 
-		void SiblingSolver::index_proc(ProcAst *proc) {
-			index_statement_list(proc->get_statement());
-		}
+void SiblingSolver::index_proc(ProcAst *proc) {
+    index_statement_list(proc->get_statement());
+}
 
-		void SiblingSolver::index_statement_list(StatementAst *statement) {
-			StatementSet statement_set;
+void SiblingSolver::index_statement_list(StatementAst *statement) {
+    StatementSet statement_set;
 
-			//Construct the complete set on all the statements on the same
-			//nesting level in the source code
-			while(statement != NULL)
-			{
-				statement_set.insert(statement);
-				index_statement(statement);
-				statement = statement->next();
-			}
+    //Construct the complete set on all the statements on the same
+    //nesting level in the source code
+    while(statement != NULL)
+    {
+        statement_set.insert(statement);
+        index_statement(statement);
+        statement = statement->next();
+    }
 
-			//Fill up the Sibling table for statements
-			for(auto it = statement_set.begin(); it!= statement_set.end(); ++it)
-			{
-				StatementSet statement_sibling_set = statement_set;
-				statement_sibling_set.erase(*it);
-				_sibling_statement_index[*it] = statement_sibling_set;
-			}
-		}
+    //Fill up the Sibling table for statements
+    for(auto it = statement_set.begin(); it!= statement_set.end(); ++it)
+    {
+        StatementSet statement_sibling_set = statement_set;
+        statement_sibling_set.erase(*it);
+        _sibling_statement_index[*it] = statement_sibling_set;
+    }
+}
 
-		void SiblingSolver::index_statement(StatementAst *statement) {
-			switch(get_statement_type(statement)) {
-			case AssignST:
-				index_assign(statement_cast<AssignmentAst>(statement));
-				break;
+void SiblingSolver::index_statement(StatementAst *statement) {
+    switch(get_statement_type(statement)) {
+    case AssignST:
+        index_assign(statement_cast<AssignmentAst>(statement));
+        break;
 
-			case WhileST:
-				index_while(statement_cast<WhileAst>(statement));
-				break;
+    case WhileST:
+        index_while(statement_cast<WhileAst>(statement));
+        break;
 
-			case IfST:
-				index_if(statement_cast<IfAst>(statement));
-				break;
+    case IfST:
+        index_if(statement_cast<IfAst>(statement));
+        break;
 
-			default:
-				// noop
-				break;
-			}
-		}
+    default:
+        // noop
+        break;
+    }
+}
 
-		void SiblingSolver::index_while(WhileAst *while_ast) {
-			//Fill up the Sibling Table for container
-			const SimpleVariable while_variable(while_ast->get_variable()->get_name());
-			SimpleVariableAst variable_ast(while_variable);
-			_sibling_container_index[&variable_ast].insert(while_ast->get_body());
+void SiblingSolver::index_while(WhileAst *while_ast) {
+    //Fill up the Sibling Table for container
+    const SimpleVariable while_variable(while_ast->get_variable()->get_name());
+    SimpleVariableAst variable_ast(while_variable);
+    _sibling_container_index[&variable_ast].insert(while_ast->get_body());
 
-			index_statement_list(while_ast->get_body());
-		}
+    index_statement_list(while_ast->get_body());
+}
 
-		void SiblingSolver::index_if(IfAst *if_ast) {
-			//Fill up the Sibling Table for container
-			const SimpleVariable if_variable(if_ast->get_variable()->get_name());
-			SimpleVariableAst variable_ast(if_variable);
-			_sibling_container_index[&variable_ast].insert(if_ast->get_then_branch());
-			_sibling_container_index[&variable_ast].insert(if_ast->get_else_branch());
+void SiblingSolver::index_if(IfAst *if_ast) {
+    //Fill up the Sibling Table for container
+    const SimpleVariable if_variable(if_ast->get_variable()->get_name());
+    SimpleVariableAst variable_ast(if_variable);
+    _sibling_container_index[&variable_ast].insert(if_ast->get_then_branch());
+    _sibling_container_index[&variable_ast].insert(if_ast->get_else_branch());
 
-			index_statement_list(if_ast->get_then_branch());
-			index_statement_list(if_ast->get_else_branch());
-		}
+    index_statement_list(if_ast->get_then_branch());
+    index_statement_list(if_ast->get_else_branch());
+}
 
-		void SiblingSolver::index_assign(AssignmentAst *assign_ast) 
-		{
-			SimpleVariable *variable = assign_ast->get_variable();
-			SimpleVariableAst variable_ast(*variable);
+void SiblingSolver::index_assign(AssignmentAst *assign_ast) 
+{
+    SimpleVariable *variable = assign_ast->get_variable();
+    SimpleVariableAst variable_ast(*variable);
 
-			ExprAst *expr_ast = assign_ast->get_expr();
-			string expr_string = extract_string_from_expr(expr_ast);
+    ExprAst *expr_ast = assign_ast->get_expr();
+    string expr_string = extract_string_from_expr(expr_ast);
 
-			//Filling the Sibling table for expressions
-			_sibling_expression_index[&variable_ast].insert(expr_ast);
-			_sibling_expression_index[expr_ast].insert(&variable_ast);
+    //Filling the Sibling table for expressions
+    _sibling_expression_index[&variable_ast].insert(expr_ast);
+    _sibling_expression_index[expr_ast].insert(&variable_ast);
 
-			_sibling_index[variable->get_name()].insert(expr_string);
-			_sibling_index[expr_string].insert(variable->get_name());
+    _sibling_index[variable->get_name()].insert(expr_string);
+    _sibling_index[expr_string].insert(variable->get_name());
 
-			index_expr(expr_ast);
-		}
+    index_expr(expr_ast);
+}
 
-		void SiblingSolver::index_expr(ExprAst* expr_ast) 
-		{
-			if(get_expr_type(expr_ast) == BinaryOpET){
-				BinaryOpAst *binary_ops_ast = expr_cast<BinaryOpAst>(
-					expr_ast);
+void SiblingSolver::index_expr(ExprAst* expr_ast) 
+{
+    if(get_expr_type(expr_ast) == BinaryOpET){
+        BinaryOpAst *binary_ops_ast = expr_cast<BinaryOpAst>(
+            expr_ast);
 
-				ExprAst *left_expr = binary_ops_ast->get_lhs();
-				ExprAst *right_expr = binary_ops_ast->get_rhs();
+        ExprAst *left_expr = binary_ops_ast->get_lhs();
+        ExprAst *right_expr = binary_ops_ast->get_rhs();
 
-				//Filling the Sibling table for expressions
-				_sibling_expression_index[binary_ops_ast->get_lhs()].insert(
-					binary_ops_ast->get_rhs());
-				_sibling_expression_index[binary_ops_ast->get_rhs()].insert(
-					binary_ops_ast->get_lhs());
+        //Filling the Sibling table for expressions
+        _sibling_expression_index[binary_ops_ast->get_lhs()].insert(
+            binary_ops_ast->get_rhs());
+        _sibling_expression_index[binary_ops_ast->get_rhs()].insert(
+            binary_ops_ast->get_lhs());
 
-				_sibling_index[extract_string_from_expr(left_expr)].insert(
-					extract_string_from_expr(right_expr));
-				_sibling_index[extract_string_from_expr(right_expr)].insert(
-					extract_string_from_expr(left_expr));
+        _sibling_index[extract_string_from_expr(left_expr)].insert(
+            extract_string_from_expr(right_expr));
+        _sibling_index[extract_string_from_expr(right_expr)].insert(
+            extract_string_from_expr(left_expr));
 
-				index_expr(left_expr);
-				index_expr(right_expr);
-			}
-		}
+        index_expr(left_expr);
+        index_expr(right_expr);
+    }
+}
 
-		string SiblingSolver::extract_string_from_expr(ExprAst* expr_ast)
-		{
-			string expr_string;
-			switch(get_expr_type(expr_ast)){
-			case ConstantET:
-				{
-					ConstAst *constant_ast = expr_cast<ConstAst>(expr_ast);
-					int constant_value = constant_ast->get_value();
+string SiblingSolver::extract_string_from_expr(ExprAst* expr_ast)
+{
+    string expr_string;
+    switch(get_expr_type(expr_ast)){
+    case ConstantET:
+        {
+            ConstAst *constant_ast = expr_cast<ConstAst>(expr_ast);
+            int constant_value = constant_ast->get_value();
 
-					expr_string = to_string(static_cast<long long>(
-						constant_value));
+            expr_string = to_string(static_cast<long long>(
+                constant_value));
 
-					break;
-				}
+            break;
+        }
 
-			case VariableET:
-				{
-					VariableAst *variable_ast = expr_cast<VariableAst>(
-						expr_ast);
+    case VariableET:
+        {
+            VariableAst *variable_ast = expr_cast<VariableAst>(
+                expr_ast);
 
-					SimpleVariable *variable = variable_ast->get_variable();
-					expr_string = variable->get_name();
-					break;
-				}
+            SimpleVariable *variable = variable_ast->get_variable();
+            expr_string = variable->get_name();
+            break;
+        }
 
-			case BinaryOpET:
-				{
-					BinaryOpAst *binary_ops_ast = expr_cast<BinaryOpAst>(
-						expr_ast);
+    case BinaryOpET:
+        {
+            BinaryOpAst *binary_ops_ast = expr_cast<BinaryOpAst>(
+                expr_ast);
 
-					expr_string = binary_ops_ast->get_op();
-					break;
-				}
+            expr_string = binary_ops_ast->get_op();
+            break;
+        }
 
-			default:
-				break;
-			}
-			return expr_string;
-		}
+    default:
+        break;
+    }
+    return expr_string;
+}
 
-		template <>
-		bool SiblingSolver::validate<StatementAst, StatementAst>(
-			StatementAst *left, StatementAst *right)
-		{
-			if(left == right){
-				return false;
-			}
+template <>
+bool SiblingSolver::validate<StatementAst, StatementAst>(
+    StatementAst *left, StatementAst *right)
+{
+    if(left == right){
+        return false;
+    }
 
-			else{
-				StatementAst *statement_ast = left;
+    else{
+        StatementAst *statement_ast = left;
 
-				//Go through in forward manner
-				while(statement_ast->next() != NULL) {
-					if(statement_ast->next() == right) {
-						return true;
-					}
-					statement_ast = statement_ast->next();
-				}
+        //Go through in forward manner
+        while(statement_ast->next() != NULL) {
+            if(statement_ast->next() == right) {
+                return true;
+            }
+            statement_ast = statement_ast->next();
+        }
 
-				statement_ast = left;
-				while(statement_ast->prev()!= NULL){
-					if(statement_ast->prev() == right){
-						return true;
-					}
-					statement_ast = statement_ast->prev();
-				}
-				return false;
-			}
-		}
+        statement_ast = left;
+        while(statement_ast->prev()!= NULL){
+            if(statement_ast->prev() == right){
+                return true;
+            }
+            statement_ast = statement_ast->prev();
+        }
+        return false;
+    }
+}
 
-		template<>
-		bool SiblingSolver::validate<ProcAst, ProcAst>
-			(ProcAst *left, ProcAst *right){
-				if(left == right){
-					return false;
-				}
-				else if( (_ast.get_proc(left->get_name()) != NULL)
-					&& (_ast.get_proc(right->get_name()) != NULL)
-					){	//Check that both procedure is inside AST
+template<>
+bool SiblingSolver::validate<ProcAst, ProcAst>
+    (ProcAst *left, ProcAst *right){
+        if(left == right){
+            return false;
+        }
+        else if( (_ast.get_proc(left->get_name()) != NULL)
+            && (_ast.get_proc(right->get_name()) != NULL)
+            ){    //Check that both procedure is inside AST
 
-						return true;
-				}
-				else
-				{
-					return false;
-				}
-		}
+                return true;
+        }
+        else
+        {
+            return false;
+        }
+}
 
-		template<>
-		bool SiblingSolver::validate<ExprAst, ExprAst>(ExprAst *left, 
-			ExprAst *right){
-				if(left == right){
-					return false;
-				}
-				else if(_sibling_index[extract_string_from_expr(left)].count(
-					extract_string_from_expr(right)) > 0)
-				{
-					return true;
-				}
-				else 
-				{
-					return false;
-				}
-		}
+template<>
+bool SiblingSolver::validate<ExprAst, ExprAst>(ExprAst *left, 
+    ExprAst *right){
+        if(left == right){
+            return false;
+        }
+        else if(_sibling_index[extract_string_from_expr(left)].count(
+            extract_string_from_expr(right)) > 0)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+}
 
-		template<>
-		bool SiblingSolver::validate<VariableAst, ExprAst>(VariableAst *left,
-			ExprAst *right){
-				return validate<ExprAst, ExprAst>(left, right);
-		}
+template<>
+bool SiblingSolver::validate<VariableAst, ExprAst>(VariableAst *left,
+    ExprAst *right){
+        return validate<ExprAst, ExprAst>(left, right);
+}
 
-		template<>
-		bool SiblingSolver::validate<ExprAst, VariableAst>(ExprAst *left, 
-			VariableAst *right){
-				return validate<ExprAst, ExprAst>(left, right);
-		}
+template<>
+bool SiblingSolver::validate<ExprAst, VariableAst>(ExprAst *left, 
+    VariableAst *right){
+        return validate<ExprAst, ExprAst>(left, right);
+}
 
-		//Procedure
-		template<>
-		ConditionSet SiblingSolver::solve_left<ProcAst>(ProcAst *proc)
-		{
-			return proc_set_to_condition_set(_sibling_proc_index[proc]);
-		}
+//Procedure
+template<>
+ConditionSet SiblingSolver::solve_left<ProcAst>(ProcAst *proc)
+{
+    return proc_set_to_condition_set(_sibling_proc_index[proc]);
+}
 
-		template<>
-		ConditionSet SiblingSolver::solve_right<ProcAst>(ProcAst *proc)
-		{
-			return proc_set_to_condition_set(_sibling_proc_index[proc]);
-		}
+template<>
+ConditionSet SiblingSolver::solve_right<ProcAst>(ProcAst *proc)
+{
+    return proc_set_to_condition_set(_sibling_proc_index[proc]);
+}
 
-		//Statement
-		template <>
-		ConditionSet SiblingSolver::solve_left<StatementAst>(
-			StatementAst *statement)
-		{
-			return statement_set_to_condition_set(_sibling_statement_index[statement]);
-		}
+//Statement
+template <>
+ConditionSet SiblingSolver::solve_left<StatementAst>(
+    StatementAst *statement)
+{
+    return statement_set_to_condition_set(_sibling_statement_index[statement]);
+}
 
-		template <>
-		ConditionSet SiblingSolver::solve_right<StatementAst>(
-			StatementAst *statement)
-		{
-			return statement_set_to_condition_set(_sibling_statement_index[statement]);
-		}
+template <>
+ConditionSet SiblingSolver::solve_right<StatementAst>(
+    StatementAst *statement)
+{
+    return statement_set_to_condition_set(_sibling_statement_index[statement]);
+}
+
 }
 }
